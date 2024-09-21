10f6cae01f4dc50acc81e6a3fcddbeb015a1d13e
Ziqiao Wang <94594480+ziqiao-w@users.noreply.github.com>
2024-09-21 15:21:40 +0800

    Update run.sh

--- bash_scripts/run.sh
+++ bash_scripts/run.sh
@@ -39,16 +39,16 @@
 #     --save_content --save_content_every 10 \
 
 
-############################################### DiT-B/2 ~ IMNET 256 ###############################################
-# accelerate launch --multi_gpu --num_processes 8 train_flow_latent.py --exp imnet_f8_ditb2 \
-#     --dataset imagenet_256 --datadir ./data/imagenet/ \
-#     --batch_size 160 --num_epoch 1000 --label_dim 1000 \
-#     --image_size 256 --f 8 --num_in_channels 4 --num_out_channels 4 \
-#     --nf 256 --ch_mult 1 2 3 4 --attn_resolution 16 8 4 --num_res_blocks 2 \
-#     --lr 1e-4 --scale_factor 0.18215 --no_lr_decay \
-#     --model_type DiT-B/2 --num_classes 1000 --label_dropout 0.1 \
-#     --save_content --save_content_every 10 \
-#     --use_grad_checkpointing \
+############################################## DiT-B/2 ~ IMNET 256 ###############################################
+accelerate launch --multi_gpu --num_processes 8 train_flow_latent.py --exp imcdnet_f8_ditb2 \
+    --dataset imagenet_256 --datadir /data/common/ImageNet \
+    --batch_size 160 --num_epoch 1000 --label_dim 1000 \
+    --image_size 256 --f 8 --num_in_channels 4 --num_out_channels 8 \
+    --nf 256 --ch_mult 1 2 3 4 --attn_resolution 16 8 4 --num_res_blocks 2 \
+    --lr 1e-4 --scale_factor 0.18215 --no_lr_decay \
+    --model_type DiT-B/2 --num_classes 1000 --label_dropout 0.1 \
+    --save_content --save_content_every 10 \
+    --use_grad_checkpointing \
 
 
 ############################################### DiT-L/2 ~ CelebA 256 ###############################################
